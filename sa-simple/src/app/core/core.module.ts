1aa36610f98b76f27286f80045a01ec9dfaec940
Oliwier Baran <oliwier.baran@icloud.com>
2024-01-06 16:57:12 +0100

    Added first components

--- sa-simple/src/app/core/core.module.ts
+++ sa-simple/src/app/core/core.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [TopMenuComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+  ],
   exports: [TopMenuComponent],
 })
 export class CoreModule {}
