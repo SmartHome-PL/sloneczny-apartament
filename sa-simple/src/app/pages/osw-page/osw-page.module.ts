658e9f2417c95cc04ef8d5d08b5b9e365e720696
Oliwier Baran <oliwier.baran@icloud.com>
2024-01-07 15:36:38 +0100

    Added dummy data

--- sa-simple/src/app/pages/osw-page/osw-page.module.ts
+++ sa-simple/src/app/pages/osw-page/osw-page.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckInComponent } from './components/check-in/check-in.component';
 import { OswPageRoutingModule } from './osw-page-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [CheckInComponent],
-  imports: [CommonModule, OswPageRoutingModule],
+  imports: [CommonModule, OswPageRoutingModule, MatCardModule, MatButtonModule],
 })
 export class OswPageModule {}
