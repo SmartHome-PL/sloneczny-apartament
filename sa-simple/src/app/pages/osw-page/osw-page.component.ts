b0d3f9cd3f49afd12eebaf992627fda22858b5c8
Oliwier Baran <oliwier.baran@icloud.com>
2024-01-11 20:23:43 +0100

    Spliting grids

--- sa-simple/src/app/pages/osw-page/osw-page.component.ts
+++ sa-simple/src/app/pages/osw-page/osw-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import {
   MenuItem,
@@ -16,11 +16,28 @@ export class OswPageComponent {
   constructor(
     private router: Router,
     private oswMenuItemsService: OswMenuItemsService
-  ) {}
+  ) {
+    this.onResize();
+  }
 
   ngOnInit(): void {
     this.oswMenuItemsService.getMenuItems().subscribe((items) => {
       this.menuItems = items;
     });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: Event) {
+    if (typeof window !== 'undefined') {
+      const width = window.innerWidth;
+
+      if (width > 1200) {
+        this.gridColumns = 4;
+      } else if (width > 800) {
+        this.gridColumns = 3;
+      } else {
+        this.gridColumns = 2;
+      }
+    }
   }
 }
