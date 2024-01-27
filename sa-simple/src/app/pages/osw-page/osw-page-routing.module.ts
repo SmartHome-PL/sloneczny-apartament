f44711ce5ee44997812540c9cecf35c2b98ecd49
Oliwier Baran <oliwier.baran@icloud.com>
2024-01-27 16:59:29 +0100

    Cleaning

--- sa-simple/src/app/pages/osw-page/osw-page-routing.module.ts
+++ sa-simple/src/app/pages/osw-page/osw-page-routing.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CheckInComponent } from './components/check-in/check-in.component';
 import { OswPageComponent } from './osw-page.component';
-import { DirectionsComponent } from './components/directions/directions.component';
-import { AmenitiesComponent } from './components/amenities/amenities.component';
 
-const routes: Routes = [
-  { path: '', component: OswPageComponent },
-  // { path: 'check-in', component: CheckInComponent },
-  // { path: 'directions', component: DirectionsComponent },
-  // { path: 'amenities', component: AmenitiesComponent },
-];
+const routes: Routes = [{ path: '', component: OswPageComponent }];
 
 import jsonData from '../../../assets/data/osw-menu-items.json';
 import { PageBuilderComponent } from './components/page-builder/page-builder.component';
