69fa1bed47087393156c5aaccc37e107fc8692c3
Oliwier Baran <oliwier.baran@icloud.com>
2024-02-08 14:19:42 +0100

    Added translations

--- sa-simple/src/app/pages/osw-page/components/check-out/check-out.component.ts
+++ sa-simple/src/app/pages/osw-page/components/check-out/check-out.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UnlockDialogComponent } from '../unlock-dialog/unlock-dialog.component';
+import { UnlockDialogService } from '../unlock-dialog/unlock-dialog.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
-  styleUrl: './check-out.component.scss'
+  styleUrl: './check-out.component.scss',
 })
 export class CheckOutComponent {
+  carCode: string = 'obj.pages.osw.check-in.content.locked.label';
 
+  isUnlockedSubscription: Subscription;
+  isUnlocked: boolean = false;
+
+  constructor(
+    private dialog: MatDialog,
+    public unlockDialogService: UnlockDialogService,
+    private router: Router
+  ) {
+    this.isUnlockedSubscription =
+      this.unlockDialogService.isUnlocked$.subscribe((isUnlocked) => {
+        if (isUnlocked) {
+          this.unlock();
+        }
+      });
+  }
+
+  unlock() {
+    this.carCode = '##5045738#';
+    this.isUnlocked = true;
+  }
+
+  openDialog(): void {
+    this.dialog.open(UnlockDialogComponent, { maxWidth: '38rem' });
+  }
+
+  redirectTo(url: string) {
+    window.location.href = url;
+  }
 }
